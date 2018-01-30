6be2525237c997c39545e61832ded796eb4c04d5
Shenbo Xuan <xuanshenbo@hotmail.com>
2018-01-30 19:16:03 +1300

    dev - add blog section to navbar

--- src/app/shared/sections/sections.ts
+++ src/app/shared/sections/sections.ts
@@ -1,8 +1,10 @@
 const ABOUT_ME = 'about me';
 const EXPERIENCES = 'experiences';
 const CONTACT = 'contact';
+const BLOG = 'blog';
 export const SECTIONS = {
   [ABOUT_ME]: 'About Me',
   [EXPERIENCES]: 'Experiences',
-  [CONTACT]: 'Contact'
+  [CONTACT]: 'Contact',
+  [BLOG]: 'Blog'
 };
