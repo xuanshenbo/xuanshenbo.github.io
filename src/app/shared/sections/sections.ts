d55027f3fdddd5f20c75479e293c47f092305062
Shenbo Xuan <xuanshenbo@hotmail.com>
2018-02-02 21:06:20 +1300

    refactor - different strategy for Blog link, as it is another site

--- src/app/shared/sections/sections.ts
+++ src/app/shared/sections/sections.ts
@@ -1,10 +1,8 @@
 const ABOUT_ME = 'aboutMe';
 const EXPERIENCES = 'experiences';
 const CONTACT = 'contact';
-const BLOG = 'blog';
 export const SECTIONS = {
   [ABOUT_ME]: 'About Me',
   [EXPERIENCES]: 'Experiences',
-  [CONTACT]: 'Contact',
-  [BLOG]: 'Blog'
+  [CONTACT]: 'Contact'
 };
